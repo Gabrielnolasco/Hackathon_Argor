3f6fad36451996381d9376b698616eeadeb600af
Gabrielnolasco <gabrielncostasouza@outlook.com>
2022-03-23 19:09:33 -0300

    Problema na verificaçao da planilha

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import programa as prog
+import pandas as pd
+#print("Agrotec \n Escolha uma opção abaixo")
+
+#print(" 1- Inserir dados dos animais")
+#print(" 2- Inserir valores")
+#print(" 3- Apagar dados dos animais")
+#print(" 4- +")
+if (  pd.read_excel('Dados_Gastos.xls').isin):
+    print("sim")
+else:
+    print("erro")
+
--- programa.py
+++ programa.py
@@ -1,7 +1,9 @@
 from asyncio.windows_events import NULL
 from distutils.log import error
+from msilib.schema import tables
+from turtle import pd
 from xmlrpc.client import Transport
-
+import pandas as pd
 
 
 
@@ -10,9 +12,10 @@ def cadastra_vaca():
     vacincao = str(input("A vaca tomou a vacina resentimente: S/N")).upper().split()[0]
     com_filhote = input("A vaca está com filhote: S/N: ").upper()
     quant_producao = int(input("Digita a quantidade de letros produzido pela vaca: \n"))
-    salvar = input("Deseja salvar os dados: S/N ").upper().split()[0]
-    if salvar == "S":
-        print("")
+    salvar = mudar(input("Deseja salvar os dados: S/N ").upper().split()[0]) 
+    
+    if salvar == True:
+        print()
     else:
         print("ola")
 
@@ -29,21 +32,16 @@ def mudar(resp):
         return resp
 
 
-#fazer um Switch Case
-
-
-
-
-sair = False
-while ( sair == False):
+#sair = False
+#while ( sair == False):
    
    # Inicio de dados produçao: 
-    numero_vacas = int(input("Digite a quantidade de vacas que voce possui: "))
-    resp = mudar(str(input("Tem alguma que nao estão produzindo leite no momento? s/n \n")).split()[0].upper())
-    if resp == True:
-        vacas_restringidas = int(input("Digite a quantidade de vacas Restringidas: "))
-        numero_vacas = numero_vacas - vacas_restringidas
-    print(numero_vacas)
+#    numero_vacas = int(input("Digite a quantidade de vacas que voce possui: "))
+#    resp = mudar(str(input("Tem alguma que nao estão produzindo leite no momento? s/n \n")).split()[0].upper())
+#    if resp == True:
+#        vacas_restringidas = int(input("Digite a quantidade de vacas Restringidas: "))
+#        numero_vacas = numero_vacas - vacas_restringidas
+#    print(numero_vacas)
     #Calculo diário
         #Calculo Mensal
 
@@ -52,11 +50,24 @@ while ( sair == False):
 
     sair = mudar(str(input("Deseja encerrar o cadastro das vacas? s/n \n")).split()[0].upper())
     
-    case
+   
 
-def gastos():
-    val_tranporte = input
+def gastos(sair = False):
+    while (sair == False):
+        nome_gasto = str(input("Digite o gasto: ops: \n *Alimentaçao\n *Trabalhadores\n *Transporte\n *Manutenção: "))
+        quanto_gasta = float(input("Digite o valor do gasto no mês: "))
+        salvar = input("Deseja salvar os dados: S/N ").upper().split()[0]
+        salvar = mudar(salvar)
+        if  (salvar == True) :
+            dados = pd.read_excel("Dados_Gastos.xls")
+            dados = dados.append({'Gasto' : nome_gasto , 'Valor' : quanto_gasta})
+            dados.to_excel('Dados_Gastos.xls', index = False)
+            return(print(" !!! Salvamento Completo !!! "))
+        else :
+            return(print("Deseja fazer"))
 
+    sair = mudar(str(input("Deseja encerrar o cadastro das vacas? s/n \n")).split()[0].upper())
+        
     return NULL
 
 
